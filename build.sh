939fe1f94e7aceb9e02e18034876132190693e15
gfdgd_xi <3025613752@qq.com>
2023-05-27 23:16:02 +0800

    更新页面

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ files=$(ls)
 main=index.html
 cat /dev/null > $main
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>文件目录</title></head><body><h1>文件目录</h1><ul>' > $main
+echo '<li><a href="'..'">'..'</a></li>' >> $main
 for i in $files; do
   if [ $i != 'CNAME' ] && [ $i != 'index.html' ] && [ $i != 'getindex.sh' ] && [ $i != 'main.html' ]; then
     echo '<li><a href="'$i'">'$i'</a></li>' >> $main
